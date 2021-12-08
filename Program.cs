91686d47df9eca8d3f60e36935872e3c9a47e58f
Andrew Pratt <andrew.pratt@hydro.com.au>
2021-12-08 22:48:25 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReadLines().Zip(ReadLines().Skip(1), (first, second) => second > first).Count(hasDepthIncreased => hasDepthIncreased));
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> ReadLines()
+        {
+            return File.ReadLines(@"input.txt").Select(int.Parse);
+        }
+    }
+}
